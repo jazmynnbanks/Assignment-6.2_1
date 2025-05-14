0d20787772353658f3e0ad3d3241d1c79070b7bb
jazmy <jazmy@JAZ-LAPTOP>
2025-05-14 17:41:36 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Stack<int> myStack  = new Stack<int>();
+Console.WriteLine("After push:");
+myStack.Push(1);
+myStack.Push(2);
+myStack.Push(3);
+myStack.Push(4);
+
+foreach (int i in myStack)
+{
+    Console.WriteLine(i);
+}
+
+myStack.Pop();
+Console.WriteLine("After pop: ");
+
+foreach (int i in myStack)
+{
+    Console.WriteLine(i);
+}
